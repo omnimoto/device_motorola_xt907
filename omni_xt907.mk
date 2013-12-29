318d8f93dc65365206f248484040fabece553705
Validus13 <groundzeroroms@gmail.com>
2013-12-29 15:25:27 -0500

    Update and rename du_xt907.mk to omni_xt907.mk

--- omni_xt907.mk
+++ omni_xt907.mk
@@ -1,8 +1,8 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/du/config/cdma.mk)
+$(call inherit-product, vendor/omni/config/cdma.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/du/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
@@ -10,7 +10,7 @@ TARGET_SCREEN_WIDTH := 540
 
 # Release name
 PRODUCT_RELEASE_NAME := XT907
-PRODUCT_NAME := du_xt907
+PRODUCT_NAME := omni_xt907
 
 $(call inherit-product, device/motorola/xt907/full_xt907.mk)
 
